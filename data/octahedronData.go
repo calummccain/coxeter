2b33566f3a313a0b03d04b60c3f9a056c5105afa
Calum McCain <cmccain97@gmail.com>
2022-01-13 18:14:43 +0000

    octahedron updated vec4

--- data/octahedronData.go
+++ data/octahedronData.go
@@ -3,7 +3,7 @@ package data
 import (
 	"math"
 
-	"github.com/calummccain/coxeter/shared"
+	"github.com/calummccain/coxeter/vector"
 )
 
 func OctahedronData(n float64) CellData {
@@ -12,95 +12,77 @@ func OctahedronData(n float64) CellData {
 	pVal := 4.0
 
 	cos := math.Pow(math.Cos(math.Pi/n), 2)
-	sin := 1 - cos
+	sin := 1.0 - cos
 	cot := cos / sin
 
 	metric := Boundaries(n, eVal, pVal)
 
-	var vv float64
+	cf := 3.0 * cot / (1 + cot)
+	ce := 2.0 * cot
+	fe := 2.0 * (1 + cot) / 3.0
+
+	var cv, fv, ev, vv float64
 
 	if metric == 'p' {
-
-		vv = 1
-
+		cv = 1.0
+		fv = 2.0
+		ev = 1.0
+		vv = 1.0
 	} else {
-
-		vv = cot / math.Abs(1-cot)
-
+		cv = cot / (1.0 - cot)
+		fv = (1.0 + cot) / (3.0 * (1.0 - cot))
+		ev = 0.5 / (1.0 - cot)
+		vv = cot / math.Abs(1.0-cot)
 	}
 
-	var d func([4]float64) [4]float64
-
+	var d func(vector.Vec4) vector.Vec4
 	if n == 3 {
-
-		d = func(v [4]float64) [4]float64 {
-
-			return [4]float64{
-				0.5 * (v[0] + v[1] + v[2] + v[3]),
-				0.5 * (v[0] + v[1] - v[2] - v[3]),
-				0.5 * (v[0] - v[1] + v[2] - v[3]),
-				0.5 * (v[0] - v[1] - v[2] + v[3]),
+		d = func(v vector.Vec4) vector.Vec4 {
+			return vector.Vec4{
+				0.5 * (v.W + v.X + v.Y + v.Z),
+				0.5 * (v.W + v.X - v.Y - v.Z),
+				0.5 * (v.W - v.X + v.Y - v.Z),
+				0.5 * (v.W - v.X - v.Y + v.Z),
 			}
-
 		}
-
 	} else if n == 4 {
-
-		d = func(v [4]float64) [4]float64 {
-
-			return [4]float64{
-				2*v[0] - v[1] - v[2] - v[3],
-				v[0] - v[2] - v[3],
-				v[0] - v[1] - v[3],
-				v[0] - v[1] - v[2],
+		d = func(v vector.Vec4) vector.Vec4 {
+			return vector.Vec4{
+				2*v.W - v.X - v.Y - v.Z,
+				v.W - v.Y - v.Z,
+				v.W - v.X - v.Z,
+				v.W - v.X - v.Y,
 			}
-
 		}
-
 	} else {
-
-		d = func(v [4]float64) [4]float64 {
-
-			return [4]float64{
-				(6*cos-2)*(v[0]-v[1]-v[2]-v[3]) + v[0],
-				2*cos*(v[0]-v[1]-v[2]-v[3]) + v[1],
-				2*cos*(v[0]-v[1]-v[2]-v[3]) + v[2],
-				2*cos*(v[0]-v[1]-v[2]-v[3]) + v[3],
+		d = func(v vector.Vec4) vector.Vec4 {
+			return vector.Vec4{
+				(6*cos-2)*(v.W-v.X-v.Y-v.Z) + v.W,
+				2*cos*(v.W-v.X-v.Y-v.Z) + v.X,
+				2*cos*(v.W-v.X-v.Y-v.Z) + v.Y,
+				2*cos*(v.W-v.X-v.Y-v.Z) + v.Z,
 			}
-
 		}
-
 	}
 
-	var f func([4]float64) [4]float64
+	var f func(vector.Vec4) vector.Vec4
 	var a, b float64
 
 	if n == 3 {
-
 		a = Rt_2
 		b = Rt_2
-
 	} else if n == 4 {
-
-		a = 1
-		b = 1
-
+		a = 1.0
+		b = 1.0
 	} else if metric == 'e' {
-
-		a = 1
-		b = 1
-
+		a = 1.0
+		b = 1.0
 	} else {
-
-		a = math.Sqrt(math.Abs(cot / (1 - cot)))
-		b = math.Sqrt(math.Abs((1 - 2*cot) / (1 - cot)))
-
+		a = math.Sqrt(math.Abs(cot / (1.0 - cot)))
+		b = math.Sqrt(math.Abs((1.0 - 2.0*cot) / (1.0 - cot)))
 	}
-
-	f = func(v [4]float64) [4]float64 {
-
-		return [4]float64{a * v[0], b * v[1], b * v[2], b * v[3]}
-
+	f = func(v vector.Vec4) vector.Vec4 {
+		return vector.Vec4{a * v.W, b * v.X, b * v.Y, b * v.Z}
 	}
 
 	return CellData{
@@ -110,20 +92,26 @@ func OctahedronData(n float64) CellData {
 		NumFaces:        8,
 		FaceReflections: []string{"", "c", "bc", "cbc", "abc", "cabc", "bcabc", "cbcabc"},
 		OuterReflection: "d",
-		V:               [4]float64{1, 1, 0, 0},
-		E:               [4]float64{2, 1, 1, 0},
-		F:               [4]float64{3, 1, 1, 1},
-		C:               [4]float64{1, 0, 0, 0},
+		V:               vector.Vec4{1, 1, 0, 0},
+		E:               vector.Vec4{2, 1, 1, 0},
+		F:               vector.Vec4{3, 1, 1, 1},
+		C:               vector.Vec4{1, 0, 0, 0},
 		CellType:        "spherical",
-		Vv:              vv,
+		CF:              cf,
+		CE:              ce,
+		CV:              cv,
+		FE:              fe,
+		FV:              fv,
+		EV:              ev,
+		VV:              vv,
 		MetricValues:    MetricValues{E: eVal, P: pVal},
-		Vertices: [][4]float64{
-			{1, 1, 0, 0},
-			{1, -1, 0, 0},
-			{1, 0, 1, 0},
-			{1, 0, -1, 0},
-			{1, 0, 0, 1},
-			{1, 0, 0, -1},
+		Vertices: []vector.Vec4{
+			vector.Vec4{1, 1, 0, 0},
+			vector.Vec4{1, -1, 0, 0},
+			vector.Vec4{1, 0, 1, 0},
+			vector.Vec4{1, 0, -1, 0},
+			vector.Vec4{1, 0, 0, 1},
+			vector.Vec4{1, 0, 0, -1},
 		},
 		Edges: [][2]int{
 			{0, 2}, {0, 3}, {0, 4}, {0, 5},
@@ -136,14 +124,11 @@ func OctahedronData(n float64) CellData {
 			{1, 4, 2}, {1, 2, 5},
 			{1, 3, 4}, {1, 5, 3},
 		},
-		Matrices: shared.Matrices{
-			A: func(v [4]float64) [4]float64 { return [4]float64{v[0], v[2], v[1], v[3]} },
-			B: func(v [4]float64) [4]float64 { return [4]float64{v[0], v[1], v[3], v[2]} },
-			C: func(v [4]float64) [4]float64 { return [4]float64{v[0], v[1], v[2], -v[3]} },
-			D: d,
-			E: func(v [4]float64) [4]float64 { return v },
-			F: f,
-		},
-		Flip: func(v [4]float64) [4]float64 { return [4]float64{-v[0], v[1], v[2], v[3]} },
+		Amat: func(v vector.Vec4) vector.Vec4 { return vector.Vec4{v.W, v.Y, v.X, v.Z} },
+		Bmat: func(v vector.Vec4) vector.Vec4 { return vector.Vec4{v.W, v.X, v.Z, v.Y} },
+		Cmat: func(v vector.Vec4) vector.Vec4 { return vector.Vec4{v.W, v.X, v.Y, -v.Z} },
+		Dmat: d,
+		Emat: func(v vector.Vec4) vector.Vec4 { return v },
+		Fmat: f,
 	}
 }
