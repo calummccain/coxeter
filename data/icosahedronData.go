00946f5c84ab81e871399eefd42ee80154196dd5
Calum McCain <cmccain97@gmail.com>
2022-01-16 16:06:42 +0000

    icosahedron

--- data/icosahedronData.go
+++ data/icosahedronData.go
@@ -8,21 +8,22 @@ import (
 
 func IcosahedronData(n float64) CellData {
 
-	eVal := math.Pi / math.Atan(P2)
-	pVal := 10.0 / 3.0
-
-	cos := math.Pow(math.Cos(math.Pi/n), 2)
-	sin := 1 - cos
+	// Trig constants
+	cos := math.Pow(math.Cos(math.Pi/n), 2.0)
+	sin := 1.0 - cos
 	cot := cos / sin
 
+	// metric dividers
+	eVal := math.Pi / math.Atan(P2)
+	pVal := 10.0 / 3.0
 	metric := Boundaries(n, eVal, pVal)
 
+	// Goursat tetrahedron side lengths
 	cf := 3.0 * P2 * cot / (1 + cot)
 	ce := P4 * cot
 	fe := P2 * (1 + cot) / 3.0
 
 	var cv, fv, ev, vv float64
-
 	if metric == 'p' {
 		cv = 0.0
 		fv = 0.0
@@ -35,57 +36,92 @@ func IcosahedronData(n float64) CellData {
 		vv = (P2 - 1.0 + P4*cot) / math.Abs(P2+1.0-P4*cot)
 	}
 
+	// reflections
 	var d func(vector.Vec4) vector.Vec4
-
 	if n == 3 {
 		d = func(v vector.Vec4) vector.Vec4 {
 			return vector.Vec4{
-				0.5 * ((P4-1.0)*v.W - (P3-3.0*P_1)*v.Y - (P-3.0*P_3)*v.Z),
-				v.X,
-				0.5 * (P5*v.W + (2.0-P4)*v.Y - P2*v.Z),
-				0.5 * (P3*v.W - P2*v.Y + v.Z),
+				W: 0.5 * ((P4-1.0)*v.W - (P3-3.0*P_1)*v.Y - (P-3.0*P_3)*v.Z),
+				X: v.X,
+				Y: 0.5 * (P5*v.W + (2.0-P4)*v.Y - P2*v.Z),
+				Z: 0.5 * (P3*v.W - P2*v.Y + v.Z),
 			}
 		}
 	} else {
 		d = func(v vector.Vec4) vector.Vec4 {
 			return vector.Vec4{
-				(6.0*P2*cos-1.0)*v.W + (2.0*P_1-6.0*P*cos)*v.Y + (2.0*P_3-6.0*cos*P_1)*v.Z,
-				v.X,
-				2.0*P5*cos*v.W + (1.0-2.0*P4*cos)*v.Y - 2.0*P2*cos*v.Z,
-				2.0*P3*cos*v.W - 2.0*P2*cos*v.Y + (1.0-2.0*cos)*v.Z,
+				W: (6.0*P2*cos-1.0)*v.W + (2.0*P_1-6.0*P*cos)*v.Y + (2.0*P_3-6.0*cos*P_1)*v.Z,
+				X: v.X,
+				Y: 2.0*P5*cos*v.W + (1.0-2.0*P4*cos)*v.Y - 2.0*P2*cos*v.Z,
+				Z: 2.0*P3*cos*v.W - 2.0*P2*cos*v.Y + (1.0-2.0*cos)*v.Z,
 			}
 		}
 	}
 
+	// metric
 	var f func(vector.Vec4) vector.Vec4
-
-	f3_1 := P3 / 2.0
-	f3_2 := math.Sqrt(3.0*P-1.0) / 2.0
-
-	fp := 1.0 / math.Sqrt(P+2.0)
-
-	fn_1 := P3 * math.Sqrt(math.Abs(cot/(P2+1.0-P4*cot)))
-	fn_2 := math.Sqrt(math.Abs((P4*cot - 1.0) / (P2 + 1.0 - P4*cot)))
-
 	if n == 3 {
 		f = func(v vector.Vec4) vector.Vec4 {
-			return vector.Vec4{f3_1 * v.W, f3_2 * v.X, f3_2 * v.Y, f3_2 * v.Z}
-		}
-	} else if metric == 'e' {
-		f = func(v vector.Vec4) vector.Vec4 {
-			return vector.Vec4{v.W, v.X, v.Y, v.Z}
-		}
-	} else if metric == 'p' {
-		f = func(v vector.Vec4) vector.Vec4 {
-			return vector.Vec4{v.W, fp * v.X, fp * v.Y, fp * v.Z}
+			return vector.Vec4{W: P3 * 0.5 * v.W, X: 0.5 * math.Sqrt(3.0*P-1.0) * v.X, Y: 0.5 * math.Sqrt(3.0*P-1.0) * v.Y, Z: 0.5 * math.Sqrt(3.0*P-1.0) * v.Z}
 		}
 	} else {
 		f = func(v vector.Vec4) vector.Vec4 {
-			return vector.Vec4{fn_1 * v.W, fn_2 * v.X, fn_2 * v.Y, fn_2 * v.Z}
+			return vector.Vec4{
+				W: P3 * math.Sqrt(math.Abs(cot/(P2+1.0-P4*cot))) * v.W,
+				X: math.Sqrt(math.Abs((P4*cot-1.0)/(P2+1.0-P4*cot))) * v.X,
+				Y: math.Sqrt(math.Abs((P4*cot-1.0)/(P2+1.0-P4*cot))) * v.Y,
+				Z: math.Sqrt(math.Abs((P4*cot-1.0)/(P2+1.0-P4*cot))) * v.Z,
+			}
 		}
+	}
+
+	// Inner product
+	var innerProd func(vector.Vec4, vector.Vec4) float64
+	if n == 3 {
+		innerProd = func(a, b vector.Vec4) float64 {
+			return (P6*a.W*b.W - (3.0*P-1.0)*(a.X*b.X+a.Y*b.Y+a.Z*b.Z)) / 4.0
+		}
+	} else {
+		innerProd = func(a, b vector.Vec4) float64 {
+			return (P6*cot*a.W*b.W - (P4*cot-1.0)*(a.X*b.X+a.Y*b.Y+a.Z*b.Z)) / math.Abs(P2+1.0-P4*cot)
+		}
+	}
+
+	V := vector.Vec4{W: 1, X: 1, Y: P, Z: 0}
+	E := vector.Vec4{W: 1, X: 0, Y: P, Z: 0}
+	F := vector.Vec4{W: 3, X: 0, Y: P3, Z: P}
+	C := vector.Vec4{W: 1, X: 0, Y: 0, Z: 0}
+	CFE := vector.Vec4{W: 0, X: 1, Y: 0, Z: 0}
+	CFV := vector.Vec4{W: 0, X: P, Y: -1, Z: P_1}
+	CEV := vector.Vec4{W: 0, X: 0, Y: 0, Z: 1}
+	FEV := vector.Vec4{W: P*cot - P_3, X: 0, Y: P2, Z: 1}
+
+	for _, vec := range []vector.Vec4{E, F, C, CFE, CFV, CEV, FEV} {
+		vec.Scale(1.0 / math.Sqrt(math.Abs(innerProd(vec, vec))))
+	}
+
+	if metric != 'p' {
+		V.Scale(1.0 / math.Sqrt(math.Abs(innerProd(V, V))))
+	}
+	E.Scale(1.0 / math.Sqrt(math.Abs(innerProd(E, E))))
+	F.Scale(1.0 / math.Sqrt(math.Abs(innerProd(F, F))))
+	C.Scale(1.0 / math.Sqrt(math.Abs(innerProd(C, C))))
+	CFE.Scale(1.0 / math.Sqrt(math.Abs(innerProd(CFE, CFE))))
+	CFV.Scale(1.0 / math.Sqrt(math.Abs(innerProd(CFV, CFV))))
+	CEV.Scale(1.0 / math.Sqrt(math.Abs(innerProd(CEV, CEV))))
+	FEV.Scale(1.0 / math.Sqrt(math.Abs(innerProd(FEV, FEV))))
+
+	Vertices := []vector.Vec4{
+		{W: 1, X: 1, Y: P, Z: 0}, {W: 1, X: 1, Y: -P, Z: 0}, {W: 1, X: -1, Y: P, Z: 0},
+		{W: 1, X: -1, Y: -P, Z: 0}, {W: 1, X: P, Y: 0, Z: 1}, {W: 1, X: -P, Y: 0, Z: 1},
+		{W: 1, X: P, Y: 0, Z: -1}, {W: 1, X: -P, Y: 0, Z: -1}, {W: 1, X: 0, Y: 1, Z: P},
+		{W: 1, X: 0, Y: 1, Z: -P}, {W: 1, X: 0, Y: -1, Z: P}, {W: 1, X: 0, Y: -1, Z: -P},
 	}
 
 	return CellData{
+		P:           3,
+		Q:           5,
+		R:           n,
 		Metric:      metric,
 		NumVertices: 12,
 		NumEdges:    30,
@@ -95,13 +131,18 @@ func IcosahedronData(n float64) CellData {
 			"cbacbcabacbc", "bacabacbc", "bcabacbc",
 			"cbcabacbc", "abcbc", "abc", "acbc",
 			"abacabacbc", "abcabacbc", "cabcabacbc",
-			"bacbc", "abacbc", "acabacbc", "cabacbc"},
+			"bacbc", "abacbc", "acabacbc", "cabacbc",
+		},
 		OuterReflection: "d",
-		V:               vector.Vec4{1, 1, P, 0},
-		E:               vector.Vec4{1, 0, P, 0},
-		F:               vector.Vec4{3, 0, P3, P},
-		C:               vector.Vec4{1, 0, 0, 0},
 		CellType:        "spherical",
+		V:               V,
+		E:               E,
+		F:               F,
+		C:               C,
+		CFE:             CFE,
+		CFV:             CFV,
+		CEV:             CEV,
+		FEV:             FEV,
 		CF:              cf,
 		CE:              ce,
 		CV:              cv,
@@ -109,13 +150,9 @@ func IcosahedronData(n float64) CellData {
 		FV:              fv,
 		EV:              ev,
 		VV:              vv,
-		MetricValues:    MetricValues{E: eVal, P: pVal},
-		Vertices: []vector.Vec4{
-			{1, 1, P, 0}, {1, 1, -P, 0}, {1, -1, P, 0},
-			{1, -1, -P, 0}, {1, P, 0, 1}, {1, -P, 0, 1},
-			{1, P, 0, -1}, {1, -P, 0, -1}, {1, 0, 1, P},
-			{1, 0, 1, -P}, {1, 0, -1, P}, {1, 0, -1, -P},
-		},
+		EVal:            eVal,
+		PVal:            pVal,
+		Vertices:        Vertices,
 		Edges: [][2]int{
 			{0, 2}, {0, 4}, {0, 6}, {0, 8},
 			{0, 9}, {1, 3}, {1, 4}, {1, 6},
@@ -133,13 +170,13 @@ func IcosahedronData(n float64) CellData {
 			{2, 7, 9}, {3, 7, 5}, {3, 5, 10}, {3, 11, 7},
 			{4, 10, 8}, {5, 8, 10}, {6, 9, 11}, {7, 11, 9},
 		},
-		Amat: func(v vector.Vec4) vector.Vec4 { return vector.Vec4{v.W, -v.X, v.Y, v.Z} },
+		Amat: func(v vector.Vec4) vector.Vec4 { return vector.Vec4{W: v.W, X: -v.X, Y: v.Y, Z: v.Z} },
 		Bmat: func(v vector.Vec4) vector.Vec4 {
-			return vector.Vec4{v.W, 0.5 * (v.X + P_1*v.Y - P*v.Z), 0.5 * (P_1*v.X + P*v.Y + v.Z), 0.5 * (-P*v.X + v.Y - P_1*v.Z)}
+			return vector.Vec4{W: v.W, X: 0.5 * (v.X + P_1*v.Y - P*v.Z), Y: 0.5 * (P_1*v.X + P*v.Y + v.Z), Z: 0.5 * (-P*v.X + v.Y - P_1*v.Z)}
 		},
-		Cmat: func(v vector.Vec4) vector.Vec4 { return vector.Vec4{v.W, v.X, v.Y, -v.Z} },
-		Dmat: d,
-		Emat: func(v vector.Vec4) vector.Vec4 { return v },
-		Fmat: f,
+		Cmat:         func(v vector.Vec4) vector.Vec4 { return vector.Vec4{W: v.W, X: v.X, Y: v.Y, Z: -v.Z} },
+		Dmat:         d,
+		Fmat:         f,
+		InnerProduct: innerProd,
 	}
 }
---
+++ data/icosahedron_test.go
@@ -0,0 +1,162 @@
+package data
+
+import (
+	"math"
+	"testing"
+
+	"github.com/calummccain/coxeter/vector"
+)
+
+func TestIcosahedron(t *testing.T) {
+
+	for i := 3; i <= 20; i++ {
+
+		data := IcosahedronData(float64(i))
+
+		// Test reflection matrices fix correct vertices of Goursat tetrahedron
+		if vector.DistanceSquared4(data.Amat(data.C), data.C) > TestEps {
+			t.Errorf("%d cell is not fixed by A \n A(C) = %v \n C = %v", i, data.Amat(data.C), data.C)
+		}
+
+		if vector.DistanceSquared4(data.Amat(data.F), data.F) > TestEps {
+			t.Errorf("%d face is not fixed by A \n A(F) = %v \n F = %v", i, data.Amat(data.F), data.F)
+		}
+
+		if vector.DistanceSquared4(data.Amat(data.E), data.E) > TestEps {
+			t.Errorf("%d edge is not fixed by A \n A(E) = %v \n E = %v", i, data.Amat(data.E), data.E)
+		}
+
+		if vector.DistanceSquared4(data.Bmat(data.C), data.C) > TestEps {
+			t.Errorf("%d cell is not fixed by B \n B(C) = %v \n C = %v", i, data.Bmat(data.C), data.C)
+		}
+
+		if vector.DistanceSquared4(data.Bmat(data.F), data.F) > TestEps {
+			t.Errorf("%d face is not fixed by B \n B(F) = %v \n F = %v", i, data.Bmat(data.F), data.F)
+		}
+
+		if vector.DistanceSquared4(data.Bmat(data.V), data.V) > TestEps {
+			t.Errorf("%d vertex is not fixed by B \n B(V) = %v \n V = %v", i, data.Bmat(data.V), data.V)
+		}
+
+		if vector.DistanceSquared4(data.Cmat(data.C), data.C) > TestEps {
+			t.Errorf("%d cell is not fixed by C \n C(C) = %v \n C = %v", i, data.Cmat(data.C), data.C)
+		}
+
+		if vector.DistanceSquared4(data.Cmat(data.E), data.E) > TestEps {
+			t.Errorf("%d edge is not fixed by C \n C(E) = %v \n E = %v", i, data.Cmat(data.E), data.E)
+		}
+
+		if vector.DistanceSquared4(data.Cmat(data.V), data.V) > TestEps {
+			t.Errorf("%d vertex is not fixed by C \n C(V) = %v \n V = %v", i, data.Cmat(data.V), data.V)
+		}
+
+		if vector.DistanceSquared4(data.Dmat(data.F), data.F) > TestEps {
+			t.Errorf("%d face is not fixed by D \n D(F) = %v \n F = %v", i, data.Dmat(data.F), data.F)
+		}
+
+		if vector.DistanceSquared4(data.Dmat(data.E), data.E) > TestEps {
+			t.Errorf("%d edge is not fixed by D \n D(E) = %v \n E = %v", i, data.Dmat(data.E), data.E)
+		}
+
+		if vector.DistanceSquared4(data.Dmat(data.V), data.V) > TestEps {
+			t.Errorf("%d vertex is not fixed by D \n D(V) = %v \n V = %v", i, data.Dmat(data.V), data.V)
+		}
+
+		// Check that the pythagorean identities are satisfied
+		if math.Abs(data.CF*data.FE-data.CE) > TestEps {
+			t.Errorf("%d CF * FE != CE \n CF * FE = %f \n CE = %f", i, data.CF*data.FE, data.CE)
+		}
+
+		if math.Abs(data.CF*data.FV-data.CV) > TestEps {
+			t.Errorf("%d CF * FV != CV \n CF * FV = %f \n CV = %f", i, data.CF*data.FV, data.CV)
+		}
+
+		if math.Abs(data.CE*data.EV-data.CV) > TestEps {
+			t.Errorf("%d CE * EV != CV \n CE * EV = %f \n CV = %f", i, data.CE*data.EV, data.CV)
+		}
+
+		if math.Abs(data.FE*data.EV-data.FV) > TestEps {
+			t.Errorf("%d FE * EV != FV \n FE * EV = %f \n FV = %f", i, data.FE*data.EV, data.FV)
+		}
+
+		// Check that the vertices have the correct norm
+		for _, vertex := range data.Vertices {
+			if data.Metric == 's' {
+				if math.Abs(data.InnerProduct(vertex, vertex)-1.0) > TestEps {
+					t.Errorf("%d Magnitude of %v != 1 \n <v,v> = %f", i, vertex, data.InnerProduct(vertex, vertex))
+				}
+			} else if data.Metric == 'h' {
+				if math.Abs(data.InnerProduct(vertex, vertex)-1.0) > TestEps {
+					t.Errorf("%d Magnitude of %v != 1 \n <v,v> = %f", i, vertex, data.InnerProduct(vertex, vertex))
+				}
+			} else if data.Metric == 'p' {
+				if math.Abs(data.InnerProduct(vertex, vertex)) > TestEps {
+					t.Errorf("%d Magnitude of %v != 0 \n <v,v> = %f", i, vertex, data.InnerProduct(vertex, vertex))
+				}
+			} else if data.Metric == 'u' {
+				if math.Abs(data.InnerProduct(vertex, vertex)+1.0) > TestEps {
+					t.Errorf("%d Magnitude of %v != -1 \n <v,v> = %f", i, vertex, data.InnerProduct(vertex, vertex))
+				}
+			}
+		}
+
+		// Check that the distances between vertices bounding an edge are correct
+		for _, edge := range data.Edges {
+			if math.Abs(data.DistanceSquared(data.Vertices[edge[0]], data.Vertices[edge[1]])-math.Pow(data.VV, 2.0)) > TestEps {
+				t.Errorf("%d Inner product between %v and %v doesn't equal %f \n <a,b> = %f", i, data.Vertices[edge[0]], data.Vertices[edge[1]], data.VV, data.DistanceSquared(data.Vertices[edge[0]], data.Vertices[edge[1]]))
+			}
+		}
+
+		// Check that the Goursat tetrahedron has the correct edge lengths
+		if math.Abs(data.DistanceSquared(data.C, data.F)-data.CF) > TestEps {
+			t.Errorf("%d Distance squared between C and F equals %f not %f", i, data.DistanceSquared(data.C, data.F), data.CF)
+		}
+
+		if math.Abs(data.DistanceSquared(data.C, data.E)-data.CE) > TestEps {
+			t.Errorf("%d Distance squared between C and E equals %f not %f", i, data.DistanceSquared(data.C, data.E), data.CE)
+		}
+
+		if math.Abs(data.DistanceSquared(data.C, data.V)-data.CV) > TestEps {
+			t.Errorf("%d Distance squared between C and V equals %f not %f", i, data.DistanceSquared(data.C, data.V), data.CV)
+		}
+
+		if math.Abs(data.DistanceSquared(data.F, data.E)-data.FE) > TestEps {
+			t.Errorf("%d Distance squared between F and E equals %f not %f", i, data.DistanceSquared(data.F, data.E), data.FE)
+		}
+
+		if math.Abs(data.DistanceSquared(data.F, data.V)-data.FV) > TestEps {
+			t.Errorf("%d Distance squared between F and V equals %f not %f", i, data.DistanceSquared(data.F, data.V), data.FV)
+		}
+
+		if math.Abs(data.DistanceSquared(data.E, data.V)-data.EV) > TestEps {
+			t.Errorf("%d Distance squared between E and V equals %f not %f", i, data.DistanceSquared(data.E, data.V), data.EV)
+		}
+
+		// Correct Goursat tetrahedron angles
+		if math.Abs(data.DistanceSquared(data.CFE, data.CFV)-math.Pow(math.Cos(math.Pi/data.P), 2.0)) > TestEps {
+			t.Errorf("%d Angle between CFE and CFV is not pi/%f", i, data.P)
+		}
+
+		if math.Abs(data.DistanceSquared(data.CFE, data.FEV)) > TestEps {
+			t.Errorf("%d Angle between CFE and FEV is not pi/2", i)
+		}
+
+		if math.Abs(data.DistanceSquared(data.CFE, data.CEV)) > TestEps {
+			t.Errorf("%d Angle between CFE and CEV is not pi/2", i)
+		}
+
+		if math.Abs(data.DistanceSquared(data.CFV, data.CEV)-math.Pow(math.Cos(math.Pi/data.Q), 2.0)) > TestEps {
+			t.Errorf("%d Angle between CFV and CEV is not pi/%f", i, data.Q)
+		}
+
+		if math.Abs(data.DistanceSquared(data.CFV, data.FEV)) > TestEps {
+			t.Errorf("%d Angle between CFV and FEV is not pi/2", i)
+		}
+
+		if math.Abs(data.DistanceSquared(data.CEV, data.FEV)-math.Pow(math.Cos(math.Pi/data.R), 2.0)) > TestEps {
+			t.Errorf("%d Angle between CEV and FEV is not pi/%f", i, data.R)
+		}
+
+	}
+
+}
