3df8651851d6f142583c35db1e5099f25dabb522
Calum McCain <cmccain97@gmail.com>
2022-02-19 18:25:04 +0000

    initialise matrix3

--- vector/matrix3.go
+++ vector/matrix3.go
@@ -1,5 +1,8 @@
 package vector
 
+// XX XY XZ
+// YX YY YZ
+// ZX ZY ZZ
 type Mat3 struct {
 	XX float64
 	XY float64
@@ -10,6 +13,20 @@ type Mat3 struct {
 	ZX float64
 	ZY float64
 	ZZ float64
+}
+
+func IniitialiseMat3(u, v, w Vec3) Mat3 {
+	return Mat3{
+		XX: u.X,
+		XY: u.Y,
+		XZ: u.Z,
+		YX: v.X,
+		YY: v.Y,
+		YZ: v.Z,
+		ZX: w.X,
+		ZY: w.Y,
+		ZZ: w.Z,
+	}
 }
 
 func (m *Mat3) Determinant() float64 {
