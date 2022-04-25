97a5a96d508690fb9c75a380e887a88a21840021
Calum McCain <cmccain97@gmail.com>
2022-04-25 20:11:46 +0100

    bug in scale

--- data/34n.go
+++ data/34n.go
@@ -21,7 +21,7 @@ func GoursatTetrahedron34n(n float64) GoursatTetrahedron {
 		C:      vector.Vec4{W: 1, X: 0, Y: 0, Z: 0},
 		Metric: "s",
 		Scale: func(v vector.Vec4) vector.Vec4 {
-			return vector.Vec4{W: v.W * Rt2, X: v.X * ipVal, Y: v.Y * ipVal, Z: v.Z * ipVal}
+			return vector.Vec4{W: v.W, X: v.X * ipVal, Y: v.Y * ipVal, Z: v.Z * ipVal}
 		},
 	}
 
