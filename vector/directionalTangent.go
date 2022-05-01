0d865ae19a9177201d4c1b5c2167dfcbb867d20a
Calum McCain <cmccain97@gmail.com>
2022-05-01 14:10:26 +0100

    directional tangent

--- vector/directionalTangent.go
+++ vector/directionalTangent.go
@@ -7,6 +7,6 @@ func DirectionalTangent(a, b Vec4) Vec4 {
 
 	ab := a.Dot(b)
 
-	return Scale4(Diff4(b, Scale4(a, ab)), 1.0/math.Sqrt(1-ab*ab))
+	return Scale4(Diff4(b, Scale4(a, ab)), 1.0/math.Sqrt(math.Abs(1-ab*ab)))
 
 }
